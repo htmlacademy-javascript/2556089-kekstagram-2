d8556990bbceb864fa5c79346cfff953dba2daa4
Mikhail Voronov <mono.vmv@gmail.com>
2025-02-03 23:59:09 +0300

    Исправляет замечания по итогам разбора ДЗ на созвоне.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-//import {createPosts} from './create-array-posts.js';
-import ('./create-thumbnails');
+import { createPosts } from './create-array-posts.js';
+import { renderThumbnails } from './create-thumbnails.js';
+
+const createdArray = createPosts();
+renderThumbnails(createdArray);
 
 
