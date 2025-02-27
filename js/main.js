976c88d7ef885bd0467cc1588373525de99a1780
Mikhail Voronov <mono.vmv@gmail.com>
2025-02-27 10:39:17 +0300

    мелкие правки в main,js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-// import { createPosts } from './create-array-posts.js';
-
 import { openUploadFormPhoto } from './form.js';
 import {loadDataFromServer} from './load-data-from-server.js';
 
 loadDataFromServer ();
-// const createdArray = createPosts();
-
 openUploadFormPhoto();
